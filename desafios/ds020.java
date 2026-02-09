59810bdb0b06c1c2be6ac0e272c5d5d41c441eaa
JoaoP-Mendes <asstecmendes@gmail.com>
2026-02-09 14:32:41 -0400

    Commit 4

--- desafios/ds020.java
+++ desafios/ds020.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
 public class ds020 {
-    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        List<String> lista = new ArrayList<>();
+
+        System.out.print("Adicione um nome na lista: ");
+        lista.add(input.next());
+
+        System.out.print("Adicione um nome na lista: ");
+        lista.add(input.next());
+
+        System.out.print("Adicione um nome na lista: ");
+        lista.add(input.next());
+
+        System.out.print("Adicione um nome na lista: ");
+        lista.add(input.next());
+
+        System.out.print("Adicione um nome na lista: ");
+        lista.add(input.next());
+
+        System.out.print(lista);
+        
+    }
 }
